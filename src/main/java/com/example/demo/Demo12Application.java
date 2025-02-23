483e27b612c4c391f324410fe90ec7c10bc500d4
Platinum <bengaminfein@gmail.com>
2025-02-23 13:16:08 +0100

    Added .env file. Created new variables, such as logins and passwords. Added these variables to application.properties.

--- src/main/java/com/example/demo/Demo12Application.java
+++ src/main/java/com/example/demo/Demo12Application.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -12,6 +13,8 @@ import org.springframework.stereotype.Component;
 @EntityScan(basePackages = "com.example.demo.model")
 public class Demo12Application {
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
         SpringApplication.run(Demo12Application.class, args);
     }
 }
