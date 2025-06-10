1bb565b328b57ebdc356e91df15cf75fdd46dd01
Platinum <bengaminfein@gmail.com>
2025-06-10 23:15:29 +0200

    Minor changes

--- src/main/java/com/example/demo/model/ConfirmationToken.java
+++ src/main/java/com/example/demo/model/ConfirmationToken.java
@@ -21,7 +21,7 @@ public class ConfirmationToken {
     private LocalDateTime createdAt;
     private LocalDateTime expiresAt;
 
-    @OneToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 }
