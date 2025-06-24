91a9dbb4126759050115b1899980d5082e59fa5a
Platinum <bengaminfein@gmail.com>
2025-06-24 22:33:54 +0200

    Minor changes.

--- src/main/java/com/example/demo/Demo12Application.java
+++ src/main/java/com/example/demo/Demo12Application.java
@@ -31,7 +31,7 @@ public class Demo12Application {
 
         System.out.println("[DEBUG]EMAIL_PASSWORD = " + dotenv.get("EMAIL_PASSWORD"));
         dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
-        System.out.println("frontend.url from System: " + System.getProperty("frontend.url"));
+        System.out.println("frontend.url from System: " + System.getProperty("frontend_url"));
         SpringApplication app = new SpringApplication(Demo12Application.class);
         app.setDefaultProperties(envProps);
         app.run(args);
